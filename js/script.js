317bd3a1497ecb1a83bcdf87744526ec49831253
chekolovec <chekolovec@gmail.com>
2019-05-02 11:08:12 +0300

    Поправил скрипт таймера

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-var timeend= new Date(2019, 4, 4);
+var timeend= new Date();
+timeend.setDate(timeend.getDate() + 1);
+timeend.setHours(0, 0, 0);
+console.log(timeend);
 // IE и FF по разному отрабатывают getYear()
 // timeend= new Date(timeend.getYear()>1900?(timeend.getYear()+1):(timeend.getYear()+1901),0,1);
 // для задания обратного отсчета до определенной даты укажите дату в формате:
@@ -10,8 +13,11 @@ function time() {
 	today = Math.floor((timeend-today)/1000);
 	var tsec=today%60; today=Math.floor(today/60); if(tsec<10)tsec='0'+tsec;
 	var tmin=today%60; today=Math.floor(today/60); if(tmin<10)tmin='0'+tmin;
+	var thour=today%24;
 	today=Math.floor(today/24);
-	var thour=today%24 + today*24;
+	if (today > 0){
+		thour += today*24;
+	};
 
 	document.getElementById('hours-left').innerHTML=thour;
 	document.getElementById('minutes-left').innerHTML=tmin;
